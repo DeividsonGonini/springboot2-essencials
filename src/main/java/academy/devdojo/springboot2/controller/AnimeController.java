eea2a7c048032e76fa9264ec0426236f3f83acf8
DeividsonGonini <deividsongonini@gmail.com>
2022-03-12 23:44:49 -0300

    Implementacao metodo GET parte 01

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,7 +1,9 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +13,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController//Especifica que a classe é um controller e adiciona o @ResponseBody(o retorno dos metodos dessa classe sao apenas String)
-@RequestMapping("anime")//URL da Classe
+@RequestMapping("animes")//URL da Classe
 @Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
-    private DateUtil dateUtil;
-
-    public AnimeController(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
-    }
 
     //localhost:8080/anime/list
     @GetMapping("list")//Metodo passando URL do Metodo
     public List<Anime> animes(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of( new Anime("DBZ"), new Anime("One Piece"));
+        return animeService.listAll();
     }
+
 }
