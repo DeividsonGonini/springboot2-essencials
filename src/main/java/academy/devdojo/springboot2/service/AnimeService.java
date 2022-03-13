fe7ff16445a2db5822d4f0209b8927a0d9e68d2a
DeividsonGonini <deividsongonini@gmail.com>
2022-03-13 00:29:05 -0300

    Adicionado get com filtro (id)

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,7 +1,9 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -9,7 +11,17 @@ import java.util.List;
 public class AnimeService {
     //private final AnimeRepository animeRepository;
 
+    private List<Anime> animes =List.of( new Anime(1L,"DBZ"), new Anime(2L,"One Piece"));
+
     public List<Anime> listAll() {
-        return List.of( new Anime(1L,"DBZ"), new Anime(2L,"One Piece"));
+        return animes;
     }
+
+    public Anime findById(long id) {
+        return animes.stream()//Varre a lista
+                .filter(anime -> anime.getId().equals(id))//filtro passando o ID
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime não encontrado"));// Em caso de erro  Retorna Bad Request e mensagem.
+    }
+
 }
