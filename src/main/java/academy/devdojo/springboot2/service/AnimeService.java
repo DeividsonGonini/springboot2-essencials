857e6c5f771510c1197f12d77e5a2e67cedaee49
DeividsonGonini <deividsongonini@gmail.com>
2022-03-22 01:36:45 -0300

    adicionado excecao customizada

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
@@ -28,7 +29,7 @@ public class AnimeService {
 
     public Anime findByIdOrThrowBadRequestException(long id) {
         return animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime não encontrado"));// Em caso de erro  Retorna Bad Request e mensagem.
+                .orElseThrow(() -> new BadRequestException("Anime não encontrado"));// Em caso de erro  Retorna Bad Request e mensagem.
     }
 
     public Anime save( AnimePostRequestBody animePostRequestBody) {
