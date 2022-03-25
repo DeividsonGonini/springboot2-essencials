42009dfdcf2914154b6beaeeb57852bdc9685c5f
DeividsonGonini <deividsongonini@gmail.com>
2022-03-25 01:18:49 -0300

    inicio aula

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -17,12 +17,20 @@ public class SpringClient {
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8081/animes/{id}", Anime.class,1);
         log.info(entity);
 
+        //##########################################//
+        //                 GET                      //
+        //##########################################//
+
         Anime object = new RestTemplate().getForObject("http://localhost:8081/animes/{id}", Anime.class,1);
         log.info(object);
 
         //Listando tudo Utilizando Array
         Anime[] animes = new RestTemplate().getForObject("http://localhost:8081/animes/all", Anime[].class);
         log.info(Arrays.toString(animes));
+
+        //##########################################//
+        //         Exchange com GET                 //
+        //##########################################//
 
         //Chamada com Exchange para pegar a resposta e converter automaticamente para uma lista
         //@formatter:off
@@ -33,5 +41,13 @@ public class SpringClient {
         //@formatter:on
         log.info(exchange.getBody());
 
+
+        //##########################################//
+        //               POST                       //
+        //##########################################//
+
+        Anime kingdom = Anime.builder().name("Kingdom").build();
+        Anime kingdomSalvo = new RestTemplate().postForObject("http://localhost:8081/animes/", kingdom, Anime.class);
+        log.info("Anime Salvo: {}", kingdomSalvo);
     }
 }
