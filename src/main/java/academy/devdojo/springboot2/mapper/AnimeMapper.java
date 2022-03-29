0493fae9a3cefdef0f175827b7a475d112eef988
DeividsonGonini <deividsongonini@gmail.com>
2022-03-28 21:56:10 -0300

    Correcao MapStruct post

--- src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
+++ src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
@@ -4,12 +4,14 @@ import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public abstract class AnimeMapper {
     public static final AnimeMapper INSTANCIA = Mappers.getMapper(AnimeMapper.class);
 
+    @Mapping( source = "name" , target = "name" )
     public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
 
     public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
