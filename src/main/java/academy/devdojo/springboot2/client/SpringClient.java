cf2fa326fad776cf703cd00c297a871156ad5c49
DeividsonGonini <deividsongonini@gmail.com>
2022-03-31 23:20:35 -0300

    adicionado rest template post e post com exchange

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -4,8 +4,7 @@ package academy.devdojo.springboot2.client;
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -17,9 +16,9 @@ public class SpringClient {
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8081/animes/{id}", Anime.class,1);
         log.info(entity);
 
-        //##########################################//
+         //##########################################//
         //                 GET                      //
-        //##########################################//
+       //##########################################//
 
         Anime object = new RestTemplate().getForObject("http://localhost:8081/animes/{id}", Anime.class,1);
         log.info(object);
@@ -28,9 +27,9 @@ public class SpringClient {
         Anime[] animes = new RestTemplate().getForObject("http://localhost:8081/animes/all", Anime[].class);
         log.info(Arrays.toString(animes));
 
-        //##########################################//
-        //         Exchange com GET                 //
-        //##########################################//
+         //##########################################//
+        //         GET com Exchange                 //
+       //##########################################//
 
         //Chamada com Exchange para pegar a resposta e converter automaticamente para uma lista
         //@formatter:off
@@ -42,12 +41,29 @@ public class SpringClient {
         log.info(exchange.getBody());
 
 
-        //##########################################//
+         //##########################################//
         //               POST                       //
-        //##########################################//
+       //##########################################//
 
-        Anime kingdom = Anime.builder().name("Kingdom").build();
-        Anime kingdomSalvo = new RestTemplate().postForObject("http://localhost:8081/animes/", kingdom, Anime.class);
-        log.info("Anime Salvo: {}", kingdomSalvo);
+        //com postForObject
+//        Anime kingdom = Anime.builder().name("Kingdom2").build();
+//        Anime kingdomSalvo = new RestTemplate().postForObject("http://localhost:8081/animes/", kingdom, Anime.class);
+//        log.info("Anime Salvo: {}", kingdomSalvo);
+
+         //##########################################//
+        //           POST com Exchange              //
+       //##########################################//        Anime samuraiChamploo = Anime.builder().name("SamuraiChamploo").build();
+        ResponseEntity<Anime> samuraiChamplooSalvo = new RestTemplate().exchange("http://localhost:8081/animes/",
+                HttpMethod.POST,
+                new HttpEntity<>(samuraiChamploo, createJsonHeader()),
+                Anime.class);
+        log.info("Anime Salvo: {}", samuraiChamplooSalvo);
+    }
+
+    //Metodo para Enviar Headers na requisição
+    private static HttpHeaders createJsonHeader(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
